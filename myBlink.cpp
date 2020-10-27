97674d277c176f58869bb1248df6b1eaac6efa0c
Demo User <debian@beaglebone.localdomain>
2020-10-27 22:03:39 +0000

    Turgeon HW6-2

---
+++ myBlink.cpp
@@ -0,0 +1,36 @@
+//Third File
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
+using namespace std;
+
+#include "derek_LED.h"
+
+int main(int argc, char* argv[]){
+   if(argc!=4){
+        cout << "Usage is makeLEDs <command>" << endl;
+        cout << "   command is one of: on, off, flash or status" << endl;
+        cout << " e.g. makeLEDs flash" << endl;
+   }
+   cout << "Starting the makeLEDs program" << endl;
+
+   string cmd(argv[1]);
+   int LEDnum = atoi(argv[2]);
+	
+   LED leds[4] = { LED(0), LED(1), LED(2), LED(3) };
+   for(int i=0; i<=3; i++){
+      if(cmd=="on")leds[i].turnOn();
+      else if(cmd=="off")leds[i].turnOff();
+      else if(cmd=="flash")leds[i].flash("100"); //default is "50"
+      else if(cmd=="status")leds[i].outputState();
+      else if(cmd=="blink"){
+		int numberofBlinks = atoi(argv[3]);
+		leds[LEDnum].blink(numberofBlinks);
+	}
+      else{ cout << "Invalid command!" << endl; }
+   }
+   cout << "Finished the makeLEDs program" << endl;
+   return 0;
+}
+
